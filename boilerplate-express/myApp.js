ed108f8de037bc60cd08fc8451a7f758bcba3615
Iasaldo Frota <61317828+iyosaldo@users.noreply.github.com>
2025-01-03 17:15:01 +0000

    Create myApp.js

---
+++ boilerplate-express/myApp.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+
+const hello = ("Hello World");
+console.log(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
